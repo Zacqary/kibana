107585feb82a6bb0b23fbba38e12a33ec67a6490
Zacqary Xeper <zacqary.xeper@elastic.co>
2023-05-08 15:30:20 -0500

    Add maintenance windows to calendar view

--- x-pack/plugins/alerting/public/index.ts
+++ x-pack/plugins/alerting/public/index.ts
@@ -9,6 +9,7 @@ import type { PluginInitializerContext } from '@kbn/core/public';
 import { AlertingPublicPlugin } from './plugin';
 export type { PluginSetupContract, PluginStartContract } from './plugin';
 export type { AlertNavigationHandler } from './alert_navigation_registry';
+export { useFindMaintenanceWindows } from './hooks/use_find_maintenance_windows';
 
 export function plugin(initializerContext: PluginInitializerContext) {
   return new AlertingPublicPlugin(initializerContext);
